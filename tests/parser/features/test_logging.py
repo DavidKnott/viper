4f60d266863d992e5fd906a61803dffbb4eb8e16
David Knott <david1k1nott@gmail.com>
2017-09-09 14:58:31 -0600

    Move parser features tests into a features folder

---
+++ tests/parser/features/test_logging.py
@@ -0,0 +1,34 @@
+import pytest
+from tests.setup_transaction_tests import chain as s, tester as t, ethereum_utils as u, check_gas, \
+    get_contract_with_gas_estimation, get_contract
+
+
+def test_loggy_code():
+    loggy_code = """
+s: bytes <= 100
+
+def foo():
+    raw_log([], "moo")
+
+def goo():
+    raw_log([0x1234567812345678123456781234567812345678123456781234567812345678], "moo2")
+
+def hoo():
+    self.s = "moo3"
+    raw_log([], self.s)
+
+def ioo(inp: bytes <= 100):
+    raw_log([], inp)
+    """
+
+    c = get_contract(loggy_code)
+    c.foo()
+    assert s.head_state.receipts[-1].logs[0].data == b'moo'
+    c.goo()
+    assert s.head_state.receipts[-1].logs[0].data == b'moo2'
+    assert s.head_state.receipts[-1].logs[0].topics == [0x1234567812345678123456781234567812345678123456781234567812345678]
+    c.hoo()
+    assert s.head_state.receipts[-1].logs[0].data == b'moo3'
+    c.ioo(b"moo4")
+    assert s.head_state.receipts[-1].logs[0].data == b'moo4'
+    print("Passed raw log tests")
